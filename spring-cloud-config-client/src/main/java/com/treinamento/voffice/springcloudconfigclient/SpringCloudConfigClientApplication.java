f73f7c138ba3f366f08f3e3a3134cb1374cd24fe
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-06-25 23:28:41 -0300

    Add final lab complete

--- spring-cloud-config-client/src/main/java/com/treinamento/voffice/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/treinamento/voffice/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -1,12 +1,35 @@
 package com.treinamento.voffice.springcloudconfigclient;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class SpringCloudConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
-	}
+    @Value("${treinamento.curso}")
+    String curso;
+
+    @Value("${treinamento.carga.horaria}")
+    String cargaHoraria;
+    @GetMapping("/curso")
+    public ResponseEntity<JsonNode> getCurso() {
+        JsonNode response = JsonNodeFactory.instance.objectNode();
+        ((ObjectNode) response).put("curso", curso);
+        ((ObjectNode) response).put("cargaHoraria", cargaHoraria);
+        return ResponseEntity.ok(response);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
 }
